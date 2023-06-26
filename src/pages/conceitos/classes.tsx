1b11bea1e95ab60a2ad6bc71e162f9ef4c2dc901
vitordiniz25 <vitordiniz25@gmail.com>
2023-06-25 23:05:37 -0300

    Classes do tailwind

---
+++ src/pages/conceitos/classes.tsx
@@ -0,0 +1,12 @@
+export default function Classes(){
+    return(
+        <div className={`
+            flex justify-center
+            bg-sky-500 text-yellow-200
+            text-6xl h-48
+            items-center
+        `}>
+            O Tailwind usa classes o tempo todo!
+        </div>
+    )
+}
