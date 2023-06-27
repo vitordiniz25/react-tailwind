25eb2d647c1bb17293d4c3119f31681ab9953038
vitordiniz25 <vitordiniz25@gmail.com>
2023-06-27 11:54:06 -0300

    grid revisão

---
+++ src/pages/grid/inicial.tsx
@@ -0,0 +1,20 @@
+import Caixa from "@/components/Caixa";
+
+export default function Inicial() {
+    return(
+        <div className="p-7 grid 
+            grid-cols-3
+            gap-2.5"
+        >
+            <Caixa className="row-span-2">#1</Caixa>
+            <Caixa>#2</Caixa>
+            <Caixa>#3</Caixa>
+            {/* <Caixa>#4</Caixa> */}
+            <Caixa className="col-span-2">#5</Caixa>
+            {/* <Caixa>#6</Caixa> */}
+            <Caixa className="col-span-2">#7</Caixa>
+            {/* <Caixa>#8</Caixa> */}
+            <Caixa>#9</Caixa>
+        </div>
+    )
+}
